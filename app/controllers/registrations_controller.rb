eef8e564de4520165704d2200142550446d71681
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-06-29 19:40:07 +0300

    Added extra user attributes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,7 @@ class RegistrationsController < Devise::RegistrationsController
 	def account_update_params
 		params.require(:user)
 			  .permit(:email, :password, :password_confirmation, :username,
-			  		  :current_password)
+			  		  :current_password, :age, :bio, :gender)
 	end
 
 end
