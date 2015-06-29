90c6220bb74e8224177691ccbb4e9955a95d7e99
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-06-29 19:28:28 +0300

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+
+  resources :users, only: [:show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
