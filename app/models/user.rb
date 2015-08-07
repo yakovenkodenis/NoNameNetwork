999a360304d37d0ef8e6d4d1c959d1c6dca49550
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-08 02:04:11 +0300

    Added friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of   :username
+  validates_presence_of :username
   validates_uniqueness_of :username
+
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: 'Friendship',
+                                 foreign_key: 'friend_id', dependent: :destroy
+
+  def request_friendship(user_2)
+    self.friendships.create(friend: user_2)
+  end
 end
