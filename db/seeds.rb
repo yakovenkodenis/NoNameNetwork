18a8c72e7e3de0e0811ec9f63a00840688b32f1f
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-08 01:41:02 +0300

    Added db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'superman1', email: 'superman1@gmail.com',
+            password: '12345678', password_confirmation: '12345678')
+User.create(username: 'superman2', email: 'superman2@gmail.com',
+            password: '12345678', password_confirmation: '12345678')
+User.create(username: 'superman3', email: 'superman3@gmail.com',
+            password: '12345678', password_confirmation: '12345678')
+User.create(username: 'superman4', email: 'superman4@gmail.com',
+            password: '12345678', password_confirmation: '12345678')
+User.create(username: 'superman5', email: 'superman5@gmail.com',
+            password: '12345678', password_confirmation: '12345678')
+
+p 'Test users created'
