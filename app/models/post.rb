41b7e212f32c8d023b2f7eee6d623aa09e0a8c99
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-28 13:12:44 +0300

    Added post model

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+  include PublicActivity::Model
+  belongs_to :user
+  validates_presence_of :user_id, :content
+
+  auto_html_for :content do
+    html_escape
+    image
+    youtube(width: '100%', height: 250, autoplay: false)
+    link target: '_blank', rel: 'nofollow'
+    simple_format
+  end
+end
